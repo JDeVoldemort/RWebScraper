2c49af78530834f38805cb1a5417ac24fd536c20
JDeVoldemort <104032188+JDeVoldemort@users.noreply.github.com>
2023-11-08 00:34:48 -0700

    All finished! :)

--- README.md
+++ README.md
@@ -2,31 +2,30 @@
 
 I did this project as a method of practicing the R programming language and learning more about using web scrapers.
 
-{Provide a description of the software that you wrote to demonstrate the R language.}
+I developed a Web Scraper to run as a script. It pulls data from a card database and uses Rvest and Dplyr to harvest info from the raw html using the css classes and ids. It also briefly processes the data and outputs a graph and prints some data to a CSV file. 
 
 The purpose of writing this software was to try using a webscraper and use the R lauguage. 
 
 {Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
 
-[Software Demo Video](http://youtube.link.goes.here)
+[Software Demo Video](https://youtu.be/3j53zEYcF2s)
 
 # Development Environment
 
-{Describe the tools that you used to develop the software}
+I used VSCODE, R, and the libraries: rvest, dplyr, and ggplot. I also used a chrome extentions to help gather the CSS info. 
 
-{Describe the programming language that you used and any libraries.}
+I used R, and the libraries: rvest, dplyr, and ggplot.
 
 # Useful Websites
 
 {Make a list of websites that you found helpful in this project}
 
-- [Web Site Name](http://url.link.goes.here)
-- [Web Site Name](http://url.link.goes.here)
+- [Youtube tutorial](https://www.youtube.com/watch?v=IM3PDKdSR7U)
+- [What is R Article](https://www.simplilearn.com/what-is-r-article#:~:text=R%20offers%20a%20wide%20variety,for%20data%20importing%20and%20cleaning.)
 
 # Future Work
 
-{Make a list of things that you need to fix, improve, and add in the future.}
 
-- Item 1
-- Item 2
-- Item 3
+- I would like to implement a user input function to loop search the data
+- I would like to conditionally build some custom dataframes to include more and less info from categories. 
+- I'd like to implement a scaper that could use different source links without a lot of recoding. 
--- webscrape.R
+++ webscrape.R
@@ -33,7 +33,10 @@ print(cardFlavor)
 # I think I need to use a list and then convert it to a dataframe, but not sure it will be necessary
 # cardflavor does not have full 50 values, since all cards don't have flavor text. exclude it for now
 
-df <- data.frame(cardTitle, cardType, cardText, cardMana)
+df <- data.frame(cardTitle,
+cardType,
+cardText,
+cardMana)
 print(df)
 # Install and load the ggplot2 package
 # install.packages("ggplot2")
@@ -45,3 +48,100 @@ plot <- ggplot(df, aes(cardType)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis labels for readability
 print(plot)
 ggsave("plot.png", plot = plot, width = 10, height = 10, units = "in")
+creatureList <- list()
+# int datatype
+count = 0
+for (i in 1:50) {
+    #use grepl to search for creature in cardType (used like the like operator in sql)
+    if (grepl("Creature", cardType[i])) {
+        creatureList[i] <- cardTitle[i]
+        print(cardTitle[i])
+        count = count + 1
+    }
+}
+# string datatype
+creatureCount = paste("CreatureCount:", count)
+print(creatureCount)
+
+# Create a dataframe
+dm <- data.frame(
+  cardTitle = cardTitle,
+  cardType = cardType,
+  cardText = cardText
+#   cardMana = cardMana,
+#   cardFlavor = cardFlavor,
+#   cardArtist = cardArtist
+#   cardStats = cardStats,
+
+)
+
+# Write the dataframe to a CSV file
+write.csv(dm, "manaTapper.csv", row.names = FALSE)
+#List Datatype
+ListLegend <- list()
+# int datatype
+count = 0
+for (i in 1:50) {
+    #use grepl to search for creature in cardType (used like the like operator in sql)
+    if (grepl("Legendary", cardType[i])) {
+        ListLegend[i] <- cardTitle[i]
+        print(paste("Legendary:",cardTitle[i]))
+        count = count + 1
+    }
+}
+# string datatype
+LegendaryCount = paste("LegendaryCount:", count)
+print(LegendaryCount)
+
+# check for string in cardTitle from legend list
+SearchPhrase <- "Orochi"
+searchCount = 0
+# for (i in ListLegend) {
+#     if (grepl(SearchPhrase, i)) {
+#         print(paste(SearchPhrase , i))
+#         searchCount = searchCount + 1
+#     }
+# }
+isFound <- FALSE #logical datatype
+complexNumber <- 3+6i  # complex
+for (i in 1:length(ListLegend)) {
+    if (!is.null(ListLegend[i]) && grepl("Search Phrase: ",SearchPhrase, ListLegend[i])) {
+        print(paste(SearchPhrase , ListLegend[i]))
+        searchCount = searchCount + 1
+        isFound <- TRUE #update isFound to true if found
+    }
+}
+if (!isFound) {
+    print(paste(SearchPhrase, "not found")) #print not found if not
+}
+print(paste(SearchPhrase,"- SearchCount:", searchCount))
+datatypes <- list()
+
+dtypeCardType <- class(cardType)
+dtypeCardText <- class(cardText)
+dtypeCardMana <- class(cardMana)
+dtypeCardStats <- class(cardStats)
+dtypeCardFlavor <- class(cardFlavor)
+dtypeCardArtist <- class(cardArtist)
+dtypeComplexNumber <- class(complexNumber)
+dtypeLegendaryCount <- class(LegendaryCount)
+dtypeIsFound <- class(isFound)
+dtypeListLegend <- class(ListLegend)
+datatypes <- c(datatypes,
+    dtypeCardType,
+    dtypeCardText,
+    dtypeCardMana,
+    dtypeCardStats,
+    dtypeCardFlavor,
+    dtypeCardArtist,
+    dtypeComplexNumber,
+    dtypeLegendaryCount,
+    dtypeIsFound,
+    dtypeListLegend)
+print(datatypes)
+# I didn't end up using the below code, but I wanted to keep it for reference
+# for (i in 1:50) {
+    
+#     # check datatypes. Trying a for loop to see how many I used in scraping data 
+
+# }
